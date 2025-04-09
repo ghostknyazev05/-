50677ef8b54c33eb4a4c91f217224ab58176cc4c
ghostknyazev05 <yaroslav.malyshenko713@yandex.ru>
2025-04-09 11:53:22 +0500

    Update LineSegment.cs

--- C# 2/LineSegment.cs
+++ C# 2/LineSegment.cs
@@ -4,32 +4,32 @@ namespace Section
 {
     public class LineSegment
     {
-        private double x;
-        private double y;
+        private double _x;
+        private double _y;
 
         // Свойства для доступа к координатам 
-        public double X { get => x; set => x = value; }
-        public double Y { get => y; set => y = value; }
+        public double X { get => _x; set => _x = value; }
+        public double Y { get => _y; set => _y = value; }
 
         // Конструктор
-        public LineSegment(double x, double y)
+        public LineSegment(double _x, double _y)
         {
-            this.x = x;
-            this.y = y;
+            this._x = _x;
+            this._y = _y;
         }
 
         // Метод проверки, попадает ли точка в отрезок 
         public bool Contains(double point)
         {
-            double min = Math.Min(x, y);
-            double max = Math.Max(x, y);
+            double min = Math.Min(_x, _y);
+            double max = Math.Max(_x, _y);
             return point >= min && point <= max;
         }
 
         // Перегрузка ToString() для отображения отрезка
         public override string ToString()
         {
-            return $"Отрезок:[{x},{y}]";
+            return $"Отрезок:[{_x},{_y}]";
         }
     }
 }
