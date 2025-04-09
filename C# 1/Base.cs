70a9b76c66c2b17a4852bb80fe5949ffa19e52e4
ghostknyazev05 <yaroslav.malyshenko713@yandex.ru>
2025-04-09 11:35:17 +0500

    Update Base.cs

--- C# 1/Base.cs
+++ C# 1/Base.cs
@@ -1,23 +1,23 @@
-﻿using System;
+using System;
 
 namespace GeometryNamespace
 {
     // Интерфейс 
     public interface IPrintable
     {
-        void Print(); // Метод для вывода информации
-        string ToString(); // Метод для получения строкового представления объекта
+        void Print(); 
+        string ToString(); 
     }
 
-    // Класс Base, который реализует интерфейс IPrintable
+    
     public class BaseClass : IPrintable
     {
-        // Создание защищенных полей, которые могут быть использованы в этом или производных от базового класса
+        
         protected double field1;
         protected double field2;
         protected double field3;
 
-        // Метод инициализации переменных значениями
+       
         public BaseClass(double a, double b, double c)
         {
             field1 = a;
@@ -25,7 +25,7 @@ namespace GeometryNamespace
             field3 = c;
         }
 
-        // Метод копирования значений
+        
         public BaseClass(BaseClass other)
         {
             field1 = other.field1;
@@ -33,7 +33,7 @@ namespace GeometryNamespace
             field3 = other.field3;
         }
 
-        // Метод получения последних цифр чисел
+       
         public virtual int GetMaxLastDigit()
         {
             int last1 = Math.Abs((int)field1) % 10;
@@ -42,13 +42,13 @@ namespace GeometryNamespace
             return Math.Max(last1, Math.Max(last2, last3));
         }
 
-        // Реализация метода интерфейса IPrintable
+        
         public virtual void Print()
         {
-            Console.WriteLine(ToString()); // Вывод информации о базовом классе
+            Console.WriteLine(ToString()); 
         }
 
-        // Реализация метода ToString из интерфейса IPrintable
+        
         public override string ToString()
         {
             return $"BaseClass поля: field1 = {field1}, field2 = {field2}, field3 = {field3}";
